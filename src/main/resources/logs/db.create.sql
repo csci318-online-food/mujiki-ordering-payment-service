0cf71af4e3007990253cd564f030cd5bd1938d21
Elvanss <duy.elvans12@gmail.com>
2024-09-13 20:25:50 +1000

    update payment

--- src/main/resources/logs/db.create.sql
+++ src/main/resources/logs/db.create.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS payments (
   id UUID PRIMARY KEY,
   user_id UUID,
-  bank_name VARCHAR(255),
+  holder_name VARCHAR(255),
   card_number VARCHAR(255),
   balance DOUBLE PRECISION
 );
