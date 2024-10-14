0227cbdc0a6b4697f9362092e2cb0adf35f742a2
Trung Nguyen <57174311+trungnt2910@users.noreply.github.com>
2024-10-14 23:55:33 +1100

    fix: Add correct annotations

--- src/main/java/com/csci318/microservice/payment/Domain/Entities/Payment.java
+++ src/main/java/com/csci318/microservice/payment/Domain/Entities/Payment.java
@@ -1,12 +1,19 @@
 package com.csci318.microservice.payment.Domain.Entities;
 
 import com.csci318.microservice.payment.Domain.Relations.User;
+import com.csci318.microservice.payment.Utils.Annotations.ManyToOne;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 import java.util.UUID;
 
